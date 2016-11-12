74651b82bcf5eb57aadbe324c4677cf1354c6b9c
wangrincreationsdev <ahmed.abdoulaye@gmail.com>
2016-11-12 01:02:37 -0700

    added validation users and announces

--- app/models/announce.rb
+++ app/models/announce.rb
@@ -1,7 +1,9 @@
 class Announce < ApplicationRecord
   mount_uploader :image, ImageUploader
   validate :image_size
-
+  validates :nom, :description, :prix, presence: true
+  validates :prix, numericality: { greater_than: 0 }
+  validates :image, presence: true
 
   private
     def image_size
